115b5105c213127c42b9fdef549687c585235a45
Chris Larsen <clarsendev@protonmail.com>
2020-03-29 18:03:04 -0400

    Remove GetCountryByName

--- src/rest_countries.cr
+++ src/rest_countries.cr
@@ -1,16 +1,12 @@
+require "http/client"
+
 require "./rest_countries/*"
 
-# TODO: Write documentation for `RestCountries`
 module RestCountries
   VERSION = "0.1.0"
 
   def self.getCountryByCode(code : String) : RestCountries::Country
     data = HTTP::Client.get "https://restcountries.eu/rest/v2/alpha/#{code.downcase}"
-    RestCountries::Country.from_json(data.body)
-  end
-
-  def self.getCountryByName(name : String) : RestCountries::Country
-    data = HTTP::Client.get "https://restcountries.eu/rest/v2/name/#{name.downcase}?fullText=true"
     RestCountries::Country.from_json(data.body)
   end
 end
--- test/rest_countries_test.cr
+++ test/rest_countries_test.cr
@@ -8,16 +8,10 @@ canada_file = file.gets_to_end
 file.close
 
 WebMock.stub(:get, "https://restcountries.eu/rest/v2/alpha/can").to_return(status: 200, body: canada_file)
-WebMock.stub(:get, "https://restcountries.eu/rest/v2/name/canada?fullText=true").to_return(status: 200, body: canada_file)
 
 class CountryTest < Minitest::Test
   def test_gets_country_data_by_code
     canada = RestCountries.getCountryByCode("CAN")
-    assert_equal "Ottawa", canada.capital
-  end
-
-  def test_gets_country_data_by_name
-    canada = RestCountries.getCountryByName("Canada")
     assert_equal "Ottawa", canada.capital
   end
 end
