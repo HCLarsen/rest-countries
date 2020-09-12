ad706ed46156d9b24463f728839a4f8b319b690e
Chris Larsen <clarsendev@protonmail.com>
2020-09-11 23:43:45 -0400

    Remove JSON::Mapping

--- src/rest_countries/country.cr
+++ src/rest_countries/country.cr
@@ -2,45 +2,45 @@ require "json"
 
 module RestCountries
   class Country
-    JSON.mapping(
-      name: String,
-      topLevelDomain: Array(String),
-      alpha2Code: String,
-      alpha3Code: String,
-      callingCodes: Array(String),
-      capital: String,
-      altSpellings: Array(String),
-      region: String,
-      subregion: String,
-      population: Int32,
-      latlng: Array(Float64),
-      demonym: String,
-      area: Float64,
-      gini: Float64,
-      timezones: Array(String),
-      borders: Array(String),
-      nativeName: String,
-      numericCode: String,
-      currencies: Array(Currency),
-      languages: Array(Language),
-      translations: Hash(String, String)
-    )
+    include JSON::Serializable
+
+    getter name : String
+    getter topLevelDomain : Array(String)
+    getter alpha2Code : String
+    getter alpha3Code : String
+    getter callingCodes : Array(String)
+    getter capital : String
+    getter altSpellings : Array(String)
+    getter region : String
+    getter subregion : String
+    getter population : Int32
+    getter latlng : Array(Float64)
+    getter demonym : String
+    getter area : Float64
+    getter gini : Float64
+    getter timezones : Array(String)
+    getter borders : Array(String)
+    getter nativeName : String
+    getter numericCode : String
+    getter currencies : Array(Currency)
+    getter languages : Array(Language)
+    getter translations : Hash(String, String)
 
     struct Currency
-      JSON.mapping(
-        code: String,
-        name: String,
-        symbol: String
-      )
+      include JSON::Serializable
+
+      getter code : String
+      getter name : String
+      getter symbol : String
     end
 
     struct Language
-      JSON.mapping(
-        iso639_1: String,
-        iso639_2: String,
-        name: String,
-        nativeName: String
-      )
+      include JSON::Serializable
+
+      getter iso639_1 : String
+      getter iso639_2 : String
+      getter name : String
+      getter nativeName : String
     end
   end
 end
